08939ec290be3e970e84d4c5e50c7d35e7568953
Leventbk <leventbkarakus@hotmail.com>
2023-11-14 01:02:43 -0500

    comments added

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,10 @@
 class Admin::CategoriesController < ApplicationController
+  # Enable HTTP basic authentication with the specified credentials
   http_basic_authenticate_with 
   name:  Rails.configuration.admin[:name], 
   password: Rails.configuration.admin[:password]
+
+  # Display a list of all categories in descending order of ID
   def index
     @categories = Category.order(id: :desc).all
   end
@@ -9,19 +12,23 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
-
+  
+  # Create a new category based on the submitted parameters
   def create
     @category = Category.new(category_params)
 
     if @category.save
+      # Redirect to the admin categories index page with a success notice
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
+      # Render the new category form again if save is unsuccessful
       render :new
     end
   end
 
   private
-
+  
+  # Define strong parameters for the category
   def category_params
     params.require(:category).permit(
       :name
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,34 +1,43 @@
 class Admin::ProductsController < ApplicationController
+  # Enable HTTP basic authentication with the specified credentials
   http_basic_authenticate_with 
   name: Rails.configuration.admin[:name], 
   password: Rails.configuration.admin[:password]
 
+  # Display a list of all products in descending order of ID
   def index
     @products = Product.order(id: :desc).all
   end
-
+  
+  # Render a form for creating a new product
   def new
     @product = Product.new
   end
 
+  # Create a new product based on the submitted parameters
   def create
     @product = Product.new(product_params)
 
     if @product.save
+      # Redirect to the admin products index page with a success notice
       redirect_to [:admin, :products], notice: 'Product created!'
     else
+      # Render the new product form again if save is unsuccessful
       render :new
     end
   end
 
+  # Delete a product based on the specified ID
   def destroy
     @product = Product.find params[:id]
     @product.destroy
+     # Redirect to the admin products index page with a success notice
     redirect_to [:admin, :products], notice: 'Product deleted!'
   end
 
   private
 
+  # Define strong parameters for the product
   def product_params
     params.require(:product).permit(
       :name,
